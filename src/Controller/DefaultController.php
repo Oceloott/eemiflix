823f782eaddfc85ee4942d5f533ca5b9c5282cf8
Oceloott <baptiste.blst@gmail.com>
2024-10-02 22:21:15 +0200

    update part

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route("/default", name: 'default')]
+    public function default(): Response
+    {
+        return $this->render('default.html.twig');
+    }
+}
+
+
+?>
