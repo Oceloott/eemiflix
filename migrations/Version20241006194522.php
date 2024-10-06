56017df5b3c38cf0f166695f0a1563bc880d643c
Oceloott <baptiste.blst@gmail.com>
2024-10-06 21:54:18 +0200

    add migration

--- migrations/Version20241006194522.php
+++ migrations/Version20241006194522.php
@@ -7,25 +7,21 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20241006194522 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'modification';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE playlist CHANGE update_at update_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE playlist CHANGE update_at update_at DATETIME NOT NULL');
     }
 }
