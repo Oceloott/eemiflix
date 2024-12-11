fec68b9a7f9b559e01fabe4efa7d1b261b6f573d
Oceloott <60733546+Oceloott@users.noreply.github.com>
2024-12-11 15:23:39 +0100

    connxion and auth

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -12,24 +12,28 @@ class AdminController extends AbstractController
     #[Route("/admin", name: 'admin')]
     public function admin(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('admin/admin.html.twig');
     }
 
     #[Route("/admin/add", name: 'admin_add')]
     public function adminAdd(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('admin/admin_add_films.html.twig');
     }
 
     #[Route("/admin/movies", name: 'admin_movies')]
     public function adminMovies(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('admin/admin_films.html.twig');
     }
 
     #[Route("/admin/users", name: 'admin_users')]
     public function adminUsers(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('admin/admin_users.html.twig');
     }
 
--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\Subscription;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,10 +10,8 @@ use Symfony\Component\Routing\Attribute\Route;
 class SubscriptionController extends AbstractController
 {
     #[Route("/subscription", name: 'subscription')]
-    public function subscription(Subscription $subscription): Response
-    {
-        return $this->render('subscription.html.twig', ['subscription' => $subscription]);
-    }
+    public function subscription(): Response    {
+        return $this->render('subscription.html.twig');    }
 }
 
 
